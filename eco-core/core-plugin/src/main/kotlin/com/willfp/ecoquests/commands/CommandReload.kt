2342f99c5e11a6596376e7193840237ae478695c
Exanthiax <107284021+Exanthiax@users.noreply.github.com>
2026-03-19 14:19:13 +0000

    Enhance reload message to include quest and task counts

--- eco-core/core-plugin/src/main/kotlin/com/willfp/ecoquests/commands/CommandReload.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/ecoquests/commands/CommandReload.kt
@@ -4,6 +4,8 @@ import com.willfp.eco.core.command.impl.Subcommand
 import com.willfp.eco.util.StringUtils
 import com.willfp.eco.util.toNiceString
 import com.willfp.ecoquests.plugin
+import com.willfp.ecoquests.quests.Quests
+import com.willfp.ecoquests.tasks.Tasks
 import org.bukkit.command.CommandSender
 
 object CommandReload : Subcommand(
@@ -16,6 +18,8 @@ object CommandReload : Subcommand(
         sender.sendMessage(
             plugin.langYml.getMessage("reloaded", StringUtils.FormatOption.WITHOUT_PLACEHOLDERS)
                 .replace("%time%", plugin.reloadWithTime().toNiceString())
+                .replace("%quests%", Quests.values().size.toString())
+                .replace("%tasks%", Tasks.values().size.toString())
         )
     }
 }
